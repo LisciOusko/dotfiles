04246131a883ccacb6e2ba6343fd77792ada3f4f
LisciOusko <pepinocerpl@gmail.com>
2024-03-12 12:17:05 +0100

    init

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,61 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.cmd [[colorscheme industry]]
+
+
+require "paq" {
+    "savq/paq-nvim",
+    "neovim/nvim-lspconfig",
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+}
+
+local nvim_lsp = require ("lspconfig")
+
+
+-- Suggested config from nvim-lspconfig github
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<space>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
+})
+
+
+nvim_lsp.clangd.setup {
+    cmd = {'clangd', '--background-index', '--compile-commands-dir', 'D:/systemc/excersies/build'},
+    init_options = {
+        clangdFileStatus = true,
+        clangdSemanticHighlighting = true
+    },
+    filetypes = {'c', 'cpp', 'cxx', 'cc'},
+    root_dir = function() vim.fn.getcwd() end,
+}
+
+
